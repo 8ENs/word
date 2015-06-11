39b0f4c230cd58e7b2ba499d08865d2fb44c721a
8ENs <ben@coderush.ca>
2015-06-11 12:12:20 -0700

    connected and configured mongoDB

---
+++ server/boot/create-pin-model.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+  app.dataSources.wordDB.automigrate('Pin', function(err) {
+    if (err) throw err;
+ 
+    app.models.Pin.create([
+      {coords: {lat: 49.282112, lng: -123.108313}, message: 'Vancouver', recipient: 'Jody', type: 'public', status: 'discovered'}
+    ], function(err, Pin) {
+      if (err) throw err;
+ 
+      console.log('Models created: \n', Pin);
+    });
+  });
+};
