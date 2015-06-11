15fc6420aac560ce9b5b8cda28aa5824c0c7c9bf
8ENs <ben@coderush.ca>
2015-06-11 12:54:44 -0700

    added relations between Pin & wUser

--- server/boot/create-pin-model.js
+++ server/boot/create-pin-model.js
@@ -3,11 +3,13 @@ module.exports = function(app) {
     if (err) throw err;
  
     app.models.Pin.create([
-      {coords: {lat: 49.282112, lng: -123.108313}, message: 'Vancouver', recipient: 'Jody', type: 'public', status: 'discovered'}
+      {coords: {lat: 49.282112, lng: -123.108313}, message: 'Winnipeg', recipient: 'Jody', type: 'public', status: 'discovered'}
     ], function(err, Pin) {
       if (err) throw err;
  
       console.log('Models created: \n', Pin);
     });
+
+
   });
 };
