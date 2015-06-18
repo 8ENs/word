f51353b56c7f59e7c3d8d1a46a25fb542db01581
Jody Chambers <jody@Jodys-MacBook-Pro.local>
2015-06-17 20:03:31 -0700

    pulling in pin data for jody

--- ionic/www/js/services.js
+++ ionic/www/js/services.js
@@ -1,35 +1,27 @@
-angular.module('word.services', [])
+angular.module('word.services', ['word'])
 
 .factory('Pins', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var pins = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  },{
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+  var pins = [];
+  var ctr = 0;
+  $.getJSON("/api/Pins?filter[include]=wUser&filter[where][type]=private&filter[where][recipient]=jody", function(pulledPins) {
+    pulledPins.forEach(function(onePin){
+      pins.push({
+        id: ctr,
+        name: onePin.name,
+        message: onePin.message,
+        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+      });
+      ctr++;
+    });
+  });
+
+    //   id: 0,
+    // name: 'Ben Sparrow',
+    // lastText: 'You on your way?',
+    // face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
 
   return {
     all: function() {
