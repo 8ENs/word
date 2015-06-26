8c0a3d2d93d495a5988373408f08b4be38025077
James Matsuba <james.matsuba@gmail.com>
2015-06-26 16:39:09 -0700

    android build for demo

---
+++ ionic/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,42 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.background-mode/www/background-mode.js",
+        "id": "de.appplant.cordova.plugin.background-mode.BackgroundMode",
+        "clobbers": [
+            "cordova.plugins.backgroundMode",
+            "plugin.backgroundMode"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "cordova.plugins.notification.local",
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-geolocation": "1.0.2-dev",
+    "cordova-plugin-whitelist": "1.0.0",
+    "de.appplant.cordova.plugin.background-mode": "0.6.4",
+    "de.appplant.cordova.plugin.local-notification": "0.8.1",
+    "org.apache.cordova.device": "0.3.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ ionic/platforms/android/assets/www/js/services.js
@@ -0,0 +1,44 @@
+angular.module('word.services', [])
+
+.factory('Pins', function() {
+  // no access to pos, we need the initial coords to be saved globally somewhere
+  // var initialPullPinsURL = "/api/Pins/distance?currentLat=" + pos.A + "&currentLng=" + pos.F + "&pinLat=" + pin.coords.lat + "&pinLng=" + pin.coords.lng + "filter[where][type]=public&filter[where][status]=saved";
+  console.log(pos);
+  var here = '43,-123'; // $scope.pos.A + "," + $scope.pos.F;
+  
+  currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+  if (currentUser != null){
+    initialPullPinsURL = API_HOST + "/api/Pins?filter[where][coords][near]=" + here + "&filter[include]=wUser&filter[where][or][0][type]=public&filter[where][or][1][recipient]=" + currentUser.username.toLowerCase();
+  }
+  var pins = [];
+  var ctr = 0;
+  $.getJSON(initialPullPinsURL, function(pulledPins) {
+    pulledPins.forEach(function(onePin){
+      pins.push($.extend(onePin, {
+        id: ctr,
+        internalId: onePin.id,
+        senderName: onePin.wUser.firstname,
+        message: onePin.message,
+        face: 'http://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+      }));
+      ctr++;
+    });
+  });
+
+  return {
+    all: function() {
+      return pins;
+    },
+    remove: function(pin) {
+      pins.splice(pins.indexOf(pin), 1);
+    },
+    get: function(pinId) {
+      for (var i = 0; i < pins.length; i++) {
+        if (pins[i].id === parseInt(pinId)) {
+          return pins[i];
+        }
+      }
+      return null;
+    }
+  };
+});
